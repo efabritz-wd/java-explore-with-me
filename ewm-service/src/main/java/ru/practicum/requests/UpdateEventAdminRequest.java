b6189e36b59994218b4b438a8bdae3c2c8ac143e
efabritz <fabritzekaterina@yandex.com>
2025-10-23 15:48:24 +0200

    admin update event with empty requestbody params

--- ewm-service/src/main/java/ru/practicum/requests/UpdateEventAdminRequest.java
+++ ewm-service/src/main/java/ru/practicum/requests/UpdateEventAdminRequest.java
@@ -1,6 +1,7 @@
 package ru.practicum.requests;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.micrometer.common.lang.Nullable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Size;
@@ -17,28 +18,38 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class UpdateEventAdminRequest {
+    @Nullable
     @Size(min = 20, max = 2000, message = "Annotation must be between 20 and 2000 characters")
     private String annotation;
 
+    @Nullable
     private Long category;
 
+    @Nullable
     @Size(min = 20, max = 7000, message = "Description must be between 20 and 7000 characters")
     private String description;
 
+    @Nullable
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime eventDate;
 
+    @Nullable
     private Location location;
 
+    @Nullable
     private Boolean paid;
 
+    @Nullable
     private Integer participantLimit;
 
+    @Nullable
     private Boolean requestModeration;
 
+    @Nullable
     @Enumerated(EnumType.STRING)
     private StateAction stateAction;
 
+    @Nullable
     @Size(min = 3, max = 120, message = "Title must be between 3 and 120 characters")
     private String title;
 }
