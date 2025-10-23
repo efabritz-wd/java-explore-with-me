486fad86b7e4dfd879a51f39e2630a5b3b9a7562
efabritz <fabritzekaterina@yandex.com>
2025-10-23 09:49:36 +0200

    admin update service corrections

--- ewm-service/src/main/java/ru/practicum/requests/UpdateEventAdminRequest.java
+++ ewm-service/src/main/java/ru/practicum/requests/UpdateEventAdminRequest.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.categories.dto.CategoryDto;
 import ru.practicum.locations.Location;
 
 import java.time.LocalDateTime;
